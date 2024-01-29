9318d3804e2588adb3ef80521de6fee759a8292f
Natnael Teshome <78238029+takeovernat@users.noreply.github.com>
2024-01-29 10:30:27 -0600

    Update settings.py

--- drinks/settings.py
+++ drinks/settings.py
@@ -16,12 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^q6v7qhulu#lyu#2nh4il#6nfb#&w6a)9-_aorq2e4yk9m8xq#'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
